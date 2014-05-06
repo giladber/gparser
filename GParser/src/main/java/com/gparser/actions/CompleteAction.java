3bd9ffd7708860710d88e60f8ea7b56113ccead3
GiladBer <oxydize@gmail.com>
2014-05-06 22:22:25 +0300

    Basic refactoring in CompleteAction

--- GParser/src/main/java/com/gparser/actions/CompleteAction.java
+++ GParser/src/main/java/com/gparser/actions/CompleteAction.java
@@ -31,27 +31,40 @@ public class CompleteAction implements ChannelAction
 	public ChannelFileData apply(ChannelFileData data)
 	{
 		List<Line> rows = data.getRowData();
-
-		Optional<Double> closestValue = rows.stream().
-			map(Line::getData).
-			filter(row -> !row.isEmpty()).
-			map(s -> Double.parseDouble(StringUtils.splitBySpaces(s)[channelNumToComplete])).
-			max((d1, d2) -> (int) (Math.abs(completeValue - d2) - Math.abs(completeValue - d1)));
-
-		if (closestValue.get() == completeValue)
+		Optional<Double> closestValue = findClosestValue(rows);
+		if (completionNotNeeded(closestValue))
 		{
 			return data;
 		}
 
-		List<Line> completedRows = rows.stream().
-			filter(row -> Double.parseDouble(StringUtils.splitBySpaces(row.getData())[channelNumToComplete]) == closestValue.get()).
-			map(row -> changeLineParameter(row, channelNumToComplete, Double.toString(completeValue))).
-			collect(Collectors.toList());
+		List<Line> completedRows = completeRows(rows, closestValue);
 
 		completedRows.addAll(rows);
 		ChannelFileData result = ChannelFileData.create(completedRows, data.getTitles(), data.getComments());
 		logger.info("Applied complete action for channel {} with completion value {}", channelNumToComplete + 1, completeValue);
 		return result;
+	}
+
+	private boolean completionNotNeeded(Optional<Double> closestValue)
+	{
+		return closestValue.get() == completeValue;
+	}
+
+	private List<Line> completeRows(List<Line> rows, Optional<Double> closestValue)
+	{
+		return rows.stream().
+			filter(row -> Double.parseDouble(StringUtils.splitBySpaces(row.getData())[channelNumToComplete]) == closestValue.get()).
+			map(row -> changeLineParameter(row, channelNumToComplete, Double.toString(completeValue))).
+			collect(Collectors.toList());
+	}
+
+	private Optional<Double> findClosestValue(List<Line> rows)
+	{
+		return rows.stream().
+			map(Line::getData).
+			filter(row -> !row.isEmpty()).
+			map(s -> Double.parseDouble(StringUtils.splitBySpaces(s)[channelNumToComplete])).
+			max((d1, d2) -> (int) (Math.abs(completeValue - d2) - Math.abs(completeValue - d1)));
 	}
 
 	private Line changeLineParameter(Line line, int numParameter, String newValue)
